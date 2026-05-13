98e7124ae55e8c8afc6d30e596e93969c4ca163c
mmartini-prog <mmartini@dlocal.com>
2026-05-12 21:11:01 -0300

    Add rules for DLOCAL and DEMERGE payment methods

---
+++ rules_mx.py
@@ -0,0 +1,19 @@
+RULES_MX_DLOCAL = [
+    ("D LOCAL", "Banorte", "1151995728", "AA370"),
+    ("EVOPAYMX", "EVO MPGs", "70177702216", "AA376"),
+    ("ABONO VENTAS", "Hey Banregio", "220881970023", "AA374"),
+    ("MERCADO PAGOREFERENCIA", "Mercadopago", "1151995728", "AA370"),
+    ("OPENMX", "Openpay", "116158803", "AA375"),
+    ("OPENMX", "Openpay_paynet", "116158803", "AA375"),
+    ("CADENA COMERCIAL OXXO SA", "OXXO Pay", "1151995728", "AA370"),
+]
+
+RULES_MX_DEMERGE = [
+    ("D LOCAL", "Banorte", "1011320992", "AA350"),
+    ("EVOPAYMX", "EVO MPGs", "70137911173", "AA358"),
+    ("ABONO VENTAS", "Hey Banregio", "220881930013", "AA354"),
+    ("MP AGREGADOR", "Mercadopago", "1011320992", "AA350"),
+    ("OPENMX", "Openpay", "111698419", "AA356"),
+    ("OPENMX", "Openpay_paynet", "113735494", "AA357"),
+    ("CADENA COMERCIAL OXXO SA", "OXXO Pay", "1011320992", "AA350"),
+]
