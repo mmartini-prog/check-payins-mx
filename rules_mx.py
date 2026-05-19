994640320aab27982242c36978ca081ccf78079d
mmartini-prog <mmartini@dlocal.com>
2026-05-19 13:58:58 -0300

    Update rules for Dlocal and Demerge configurations

--- rules_mx.py
+++ rules_mx.py
@@ -1,21 +1,54 @@
 RULES_MX_DLOCAL = [
-    # Banorte Dlocal desactivado porque "D LOCAL" trae movimientos internos muy grandes
-    # ("D LOCAL", "Banorte", "1151995728", "AA370"),
 
+    # BANORTE
+    ("D LOCAL", "Banorte", "1151995728", "AA370"),
+
+    # EVO
     ("EVOPAYMX", "EVO MPGs", "70177702216", "AA376"),
+
+    # BANREGIO
     ("ABONO VENTAS", "Hey Banregio", "220881970023", "AA374"),
+
+    # MERCADOPAGO
     ("MERCADO PAGOREFERENCIA", "Mercadopago", "1151995728", "AA370"),
+
+    # OPENPAY
     ("OPENMX", "Openpay", "116158803", "AA375"),
+
+    # OPENPAY PAYNET
     ("OPENMX", "Openpay_paynet", "116158803", "AA375"),
+
+    # OXXO PAY
     ("CADENA COMERCIAL OXXO SA", "OXXO Pay", "1151995728", "AA370"),
+
+    # KUSHKI
+    ("LIQ DLOCALMEXICO", "Kushki", "1151995728", "AA370"),
 ]
 
+
 RULES_MX_DEMERGE = [
+
+    # BANORTE
     ("D LOCAL", "Banorte", "1011320992", "AA350"),
+
+    # EVO
     ("EVOPAYMX", "EVO MPGs", "70137911173", "AA358"),
+
+    # BANREGIO
     ("ABONO VENTAS", "Hey Banregio", "220881930013", "AA354"),
+
+    # MERCADOPAGO
     ("MP AGREGADOR", "Mercadopago", "1011320992", "AA350"),
+
+    # OPENPAY
     ("OPENMX", "Openpay", "111698419", "AA356"),
+
+    # OPENPAY PAYNET
     ("OPENMX", "Openpay_paynet", "113735494", "AA357"),
+
+    # OXXO PAY
     ("CADENA COMERCIAL OXXO SA", "OXXO Pay", "1011320992", "AA350"),
+
+    # KUSHKI
+    ("LIQ DEMEREGE BIG PLAYERS", "Kushki", "1011320992", "AA350"),
 ]
