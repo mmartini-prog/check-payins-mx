a03c82ba647583a3d837b88b4f110db3aaba80ed
mmartini-prog <mmartini@dlocal.com>
2026-05-21 15:20:35 -0300

    Update payment rules and add fallbacks in rules_mx.py

--- rules_mx.py
+++ rules_mx.py
@@ -1,48 +1,41 @@
+
 RULES_MX_DLOCAL = [
-    # BANORTE
+    # Reglas originales Dlocal Mexico
     ("D LOCAL", "Banorte", "1151995728", "AA370"),
-
-    # EVO
     ("EVOPAYMX", "EVO MPGs", "70177702216", "AA376"),
-
-    # BANREGIO
     ("ABONO VENTAS", "Hey Banregio", "220881970023", "AA374"),
-
-    # MERCADOPAGO
     ("MERCADO PAGOREFERENCIA", "Mercadopago", "1151995728", "AA370"),
-
-    # OPENPAY / PAYNET
+    ("MERCADO PAGO", "Mercadopago", "1151995728", "AA370"),
     ("OPENMX", "Openpay", "116158803", "AA375"),
-
-    # OXXO PAY
     ("CADENA COMERCIAL OXXO SA", "OXXO Pay", "1151995728", "AA370"),
-
-    # KUSHKI
     ("LIQ DLOCALMEXICO", "Kushki", "1151995728", "AA370"),
+
+    # Fallbacks por texto para cuentas nuevas cuando el Account ID no coincide
+    ("EVOPAYMX", "EVO MPGs", "", ""),
+    ("ABONO VENTAS", "Hey Banregio", "", ""),
+    ("MERCADO PAGOREFERENCIA", "Mercadopago", "", ""),
+    ("MERCADO PAGO", "Mercadopago", "", ""),
+    ("OPENMX", "Openpay", "", ""),
+    ("CADENA COMERCIAL OXXO SA", "OXXO Pay", "", ""),
+    ("LIQ DLOCALMEXICO", "Kushki", "", ""),
 ]
 
 RULES_MX_DEMERGE = [
-    # BANORTE
     ("D LOCAL", "Banorte", "1011320992", "AA350"),
-
-    # EVO
     ("EVOPAYMX", "EVO MPGs", "70137911173", "AA358"),
-
-    # BANREGIO
     ("ABONO VENTAS", "Hey Banregio", "220881930013", "AA354"),
-
-    # MERCADOPAGO
     ("MP AGREGADOR", "Mercadopago", "1011320992", "AA350"),
-
-    # OPENPAY
+    ("MERCADO PAGO", "Mercadopago", "1011320992", "AA350"),
     ("OPENMX", "Openpay", "111698419", "AA356"),
-
-    # OPENPAY PAYNET
     ("OPENMX", "Openpay_paynet", "113735494", "AA357"),
-
-    # OXXO PAY
     ("CADENA COMERCIAL OXXO SA", "OXXO Pay", "1011320992", "AA350"),
-
-    # KUSHKI
     ("LIQ DEMEREGE BIG PLAYERS", "Kushki", "1011320992", "AA350"),
-]
+
+    # Fallbacks por texto
+    ("EVOPAYMX", "EVO MPGs", "", ""),
+    ("ABONO VENTAS", "Hey Banregio", "", ""),
+    ("MP AGREGADOR", "Mercadopago", "", ""),
+    ("MERCADO PAGO", "Mercadopago", "", ""),
+    ("OPENMX", "Openpay", "", ""),
+    ("CADENA COMERCIAL OXXO SA", "OXXO Pay", "", ""),
+    ("LIQ DEMEREGE BIG PLAYERS", "Kushki", "", ""),
